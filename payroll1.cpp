3330f078402c234fa98d1975834e64edccd843d9
vatsalpatel9 <44619923+vatsalpatel9@users.noreply.github.com>
2021-08-01 22:14:49 -0500

    calculate the pays

--- payroll1.cpp
+++ payroll1.cpp
@@ -7,8 +7,9 @@
 using namespace std;
 
 void tellUser();
-
-double regular(int &);
+double getGrossPay(double hoursWorked, double rateOfPay);
+double getTaxes(double grossPay);
+double getNetPay(double grossPay, double tax);
 
 int main(){
   // size of arrays to hold employees
@@ -16,7 +17,7 @@ int main(){
 
   string fName[SZ], lName[SZ];
 
-  double hours[SZ], rate[SZ];
+  double hours[SZ], rate[SZ], grossPay[SZ], tax[SZ], netPay[SZ];
 
   int arrSize = sizeof(fName) / sizeof(fName[0]);
   cout << arrSize << endl;
@@ -43,16 +44,25 @@ int main(){
     
   outputFile.open("output.txt");
   if (outputFile.is_open()){
-    outputFile << left << "Name" << setw(15) << " " 
-               << "Hours" << setw(8) << " " 
-               << "Rate" << setw(8) << " "
-               << "Regular" << setw(8) << " "
-               << "Overtime" << setw(8) << " "
-               << "Gross\n\n";
+    outputFile << left << "Name" << setw(15) << " "
+         << "Hours" << setw(8) << " "
+         << "Rate" << setw(8) << " "
+         << "Gross" << setw(8) << " "
+         << "Taxes" << setw(8) << " "
+         << "Net\n\n";
+    for (int i = 0; i < arrSize; i++){
+      grossPay[i] = getGrossPay(hours[i], rate[i]);
+      tax[i] = getTaxes(grossPay[i]);
+      netPay[i] = getNetPay(grossPay[i], tax[i]);
+    }
+
     for (int i = 0; i < arrSize; i++){
       outputFile << setw(19) << string(fName[i] + ' ' + lName[i])
                  << setw(13) << fixed << setprecision(2) << rate[i]
-                 << hours[i] << endl;
+                 << setw(12) << hours[i]
+                 << setw(13) << grossPay[i]
+                 << setw(12) << tax[i]
+                 << netPay[i] << endl;
     }
   }else{
     screenonly = true;
@@ -63,13 +73,16 @@ int main(){
   cout << left << "Name" << setw(15) << " "
              << "Hours" << setw(8) << " "
              << "Rate" << setw(8) << " "
-             << "Regular" << setw(8) << " "
-             << "Overtime" << setw(8) << " "
-             << "Gross\n\n";
+             << "Gross" << setw(8) << " "
+             << "Taxes" << setw(8) << " "
+             << "Net\n\n";
   for (int i = 0; i < arrSize; i++){
     cout << setw(19) << string(fName[i] + ' ' + lName[i])
          << setw(13) << fixed << setprecision(2) << rate[i]
-         << hours[i] << endl;
+         << setw(12) << hours[i]
+         << setw(13) << grossPay[i]
+         << setw(12) << tax[i]
+         << netPay[i] << endl;
   }
 
     inputFile.close();
@@ -85,15 +98,21 @@ int main(){
 void tellUser()
 {
   cout << "\nThis program reads a file called hourlyInput.txt,\n"<< "and it calculates the paycheck.\n"
-  << "output is written to the screen. \n\n";
+  << "output is written to the screen. \n" 
+  << "----------------------------------------------------------------------------------------------\n";
 }
 
-double regular(int &)
-{
-  int empCount;
-  double grossPay;
-  double hours, rate;
-  if(hours <= 40)
-      grossPay = hours * rate;
-  
+double getGrossPay(double hoursWorked, double rateOfPay){
+  return hoursWorked * rateOfPay;
+}
+
+double getTaxes(double grossPay){
+  const double fedralTax = .09;
+  const double stateTax = .05;
+  const double ficaTax = .062;
+  return (grossPay * fedralTax) + (grossPay * stateTax) + (grossPay * ficaTax);
+}
+
+double getNetPay(double grossPay, double tax){
+  return grossPay - tax;
 }
